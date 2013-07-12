f375ffcda863c2a7d6c558d7298b050a480d920c
canham@CS.YORK.AC.UK <canham@CS.YORK.AC.UK@9de38aea-ec91-4ea3-8f56-587be14e846f>
2013-07-12 16:01:53 +0000

    Resolved conflicts in reactive processes

--- theories/theories/utp_reactive.thy
+++ theories/theories/utp_reactive.thy
@@ -19,7 +19,7 @@ abbreviation "tr   \<equiv> MkPlainP ''tr'' True TYPE('m EVENT ULIST) TYPE('m)"
 abbreviation "ref  \<equiv> MkPlainP ''ref'' True TYPE('m EVENT UFSET) TYPE('m)"
 
 definition SkipREA :: "'a WF_PREDICATE" where
-"SkipREA = `(\<not> ok \<and> ($tr \<le> $tr\<acute>)) \<or> (ok\<acute> \<and> II\<^bsub>REL_VAR - OKAY\<^esub>)`"
+"SkipREA = `(\<not> ok \<and> ($tr \<le> $tr\<acute>)) \<or> (ok' \<and> II\<^bsub>REL_VAR - OKAY\<^esub>)`"
 
 syntax 
   "_upred_skiprea" :: " upred" ("II\<^bsub>rea\<^esub>")
@@ -42,19 +42,65 @@ definition R2 :: "'a WF_PREDICATE \<Rightarrow> 'a WF_PREDICATE" where
 definition R3 :: "'a WF_PREDICATE \<Rightarrow> 'a WF_PREDICATE" where
 "R3(P) = `II\<^bsub>rea\<^esub> \<lhd> $wait \<rhd> P`"
 
-definition R :: "'a WF_PREDICATE \<Rightarrow> 'a WF_PREDICATE" where 
-"R P = (R1 \<circ> R2 \<circ> R3)P"
-
 syntax 
   "_upred_R1" :: "upred \<Rightarrow> upred" ("R1'(_')")
   "_upred_R2" :: "upred \<Rightarrow> upred" ("R2'(_')")
   "_upred_R3" :: "upred \<Rightarrow> upred" ("R3'(_')")
-  "_upred_R" :: "upred \<Rightarrow> upred" ("R'(_')")
 
 translations
   "_upred_R1 P" == "CONST R1 P"
   "_upred_R2 P" == "CONST R2 P"
   "_upred_R3 P" == "CONST R3 P"
+
+declare R1_def [eval, evalr, evalrr, evalrx]
+declare R2_def [eval, evalr, evalrr, evalrx]
+declare R3_def [eval, evalr, evalrr, evalrx]
+declare is_healthy_def [eval, evalr, evalrr, evalrx]
+declare SkipREA_def [eval, evalr, evalrr, evalrx]
+
+lemma R1_idempotent : "`R1(R1(P))` = `R1(P)`" by (utp_rel_tac)
+
+lemma R2_idempotent : "`R2(R2(P))` = `R2(P)`" sorry
+
+lemma R3_idempotent : "`R3(R3(P))` = `R3(P)`" by (utp_pred_auto_tac)
+
+lemma R1_monotonic : "[P \<Rightarrow> Q] \<Longrightarrow> [R1(P) \<Rightarrow> R1(Q)]" by(utp_pred_tac)
+lemma R2_monotonic : "[P \<Rightarrow> Q] \<Longrightarrow> [R2(P) \<Rightarrow> R2(Q)]" by(utp_pred_tac)
+lemma R3_monotonic : "[P \<Rightarrow> Q] \<Longrightarrow> [R3(P) \<Rightarrow> R3(Q)]" by(utp_pred_tac)
+
+lemma SkipREA_is_R1: "`II\<^bsub>rea\<^esub> \<and> ($tr \<le> $tr\<acute>)`= `II\<^bsub>rea\<^esub>`" sorry
+lemma R1_true_is_R2 : "`($tr \<le> $tr\<acute>)[($tr\<acute> - $tr)/tr\<acute>][\<langle>\<rangle>/tr]` = `($tr \<le> $tr\<acute>)`"sorry
+lemma SkipREA_is_R2 : "`II\<^bsub>rea\<^esub>[($tr\<acute> - $tr)/tr\<acute>][\<langle>\<rangle>/tr]` = `II\<^bsub>rea\<^esub>`" sorry
+lemma wait_is_R2 : "`$wait[($tr\<acute> - $tr)/tr\<acute>][\<langle>\<rangle>/tr]` = `$wait`" sorry
+
+lemma R1_R3_commute : "R1 (R3 P) = R3 (R1 P)" 
+proof - 
+  have "R1 (R3 P) = `(II\<^bsub>rea\<^esub> \<lhd> $wait \<rhd> P) \<and> ($tr \<le> $tr\<acute>)`" by (utp_rel_tac)
+  also have "... = `(II\<^bsub>rea\<^esub> \<and> ($tr \<le> $tr\<acute>)) \<lhd> $wait \<rhd> (P \<and> ($tr \<le> $tr\<acute>))`" by (utp_pred_auto_tac)
+  ultimately show ?thesis by (simp add:SkipREA_is_R1, utp_rel_tac)
+qed
+  
+lemma R1_R2_commute : "R1 (R2 P) = R2 (R1 P)" 
+proof -
+  have "R2 (R1 P) = `(P \<and> ($tr \<le> $tr\<acute>))[\<langle>\<rangle>/tr][($tr\<acute> - $tr)/tr\<acute>]`" by (utp_rel_tac)
+  also have "... = `P[\<langle>\<rangle>/tr][($tr\<acute> - $tr)/tr\<acute>] \<and> ($tr \<le> $tr\<acute>)`" sorry(*by (simp add:usubst erasure typing defined closure R1_true_is_R2)*)
+  ultimately show ?thesis by (utp_rel_tac)
+qed
+lemma R2_R3_commute : "R2 (R3 P) = R3 (R2 P)" 
+proof - 
+  have "R2 (R3 P) = `(II\<^bsub>rea\<^esub> \<lhd> $wait \<rhd> P)[\<langle>\<rangle>/tr][($tr\<acute> - $tr)/tr\<acute>]`" by (utp_rel_tac)
+  also have "... = `II\<^bsub>rea\<^esub> \<lhd> $wait \<rhd> (P[\<langle>\<rangle>/tr][($tr\<acute> - $tr)/tr\<acute>])`"sorry(* by (simp add:usubst erasure typing defined closure SkipREA_is_R2 wait_is_R2)*)
+  ultimately show ?thesis by (utp_rel_tac)
+qed
+
+definition R :: "'a WF_PREDICATE \<Rightarrow> 'a WF_PREDICATE" where 
+"R P = (R1 \<circ> R2 \<circ> R3)P"
+
+declare R_def [eval, evalr, evalrr, evalrx]
+syntax 
+  "_upred_R" :: "upred \<Rightarrow> upred" ("R'(_')")
+
+translations
   "_upred_R P" == "CONST R P"
 
 lemma SkipREA_CondR_SkipR: 
@@ -84,7 +130,6 @@ proof -
     apply (drule_tac x="tr\<down>" in bspec)
     apply (simp_all add:var_dist closure)
   done
-
   finally show ?thesis ..
 qed
 
