f30355d6e995db410e2376deacc99df8c42b0256
simonf@CS.YORK.AC.UK <simonf@CS.YORK.AC.UK@9de38aea-ec91-4ea3-8f56-587be14e846f>
2013-07-12 15:08:55 +0000

    Added missing file

---
+++ theories/poly/utp_poly.thy
@@ -0,0 +1,14 @@
+(******************************************************************************)
+(* Project: Unifying Theories of Programming in HOL                           *)
+(* File: utp_poly.thy                                                         *)
+(* Author: Simon Foster, University of York (UK)                              *)
+(******************************************************************************)
+
+header {* Meta-Theory for the Polymorphic Expression Model *}
+
+theory utp_poly
+imports 
+  utp_poly_value
+  utp_poly_var
+  utp_poly_expr
+begin end
