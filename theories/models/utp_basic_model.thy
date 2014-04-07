e517b7e52ebfbfa8c320a115cf447abae497b7d8
simonf@CS.YORK.AC.UK <simonf@CS.YORK.AC.UK@9de38aea-ec91-4ea3-8f56-587be14e846f>
2014-04-07 15:43:03 +0000

    Fixed some of the initial problems, and added a note about dependent products

--- theories/models/utp_basic_model.thy
+++ theories/models/utp_basic_model.thy
@@ -6,13 +6,18 @@ begin
 
 default_sort type
 
-datatype bval = IntV int | BoolV bool | ListV "bval list"
+datatype btyp = Int\<^isub>t | Bool\<^isub>t | List\<^isub>t btyp
+datatype bval = Bot\<^isub>v btyp | Int\<^isub>v int | Bool\<^isub>v bool | List\<^isub>v btyp "(bval list)"
 
-derive linorder bval
-
-datatype btyp = BoolT | IntT | ListT btyp
+inductive bval_type_rel :: "bval \<Rightarrow> btyp \<Rightarrow> bool" (infix ":\<^sub>b" 50) where
+Bot\<^isub>v_type [intro]: "Bot\<^isub>v(t) :\<^sub>b t" |
+Bool\<^isub>v_type [intro]: "Bool\<^isub>v(x) :\<^sub>b Bool\<^isub>t"  |  
+Int\<^isub>v_type [intro]: "Int\<^isub>v(n) :\<^sub>b Int\<^isub>t"  | 
+List\<^isub>v_type [intro]: "\<forall> x\<in>set(xs). x :\<^sub>b t \<Longrightarrow> List\<^isub>v(t)(xs) :\<^sub>b List\<^isub>t(t)"
 
 derive countable btyp
+derive linorder btyp
+derive linorder bval
 
 type_synonym 'a bexp   = "('a, bval) WF_PEXPRESSION"
 type_synonym bpred     = "bval WF_PREDICATE" 
@@ -23,28 +28,25 @@ translations
   (type) "bpred" <= (type) "bval WF_PREDICATE"
   (type) "'a bvar" <= (type) "('a, bval) PVAR"
 
-inductive bval_type_rel :: "bval \<Rightarrow> btyp \<Rightarrow> bool" (infix ":\<^sub>b" 50) where
-BoolV_type [intro]: "BoolV x :\<^sub>b BoolT" |
-IntV_type  [intro]: "IntV x :\<^sub>b IntT" |
-ListV_type [intro]: "\<forall> x\<in>set xs. x :\<^sub>b t \<Longrightarrow> ListV xs :\<^sub>b ListT t"
-
 inductive_cases
-  BoolV_cases [elim]: "BoolV x :\<^sub>b t" and
-  BoolT_cases [elim!]: "x :\<^sub>b BootT" and
-  IntV_cases [elim]: "IntV x :\<^sub>b t" and
-  IntT_cases [elim!]: "x :\<^sub>b IntT" and
-  ListV_cases [elim]: "ListV xs :\<^sub>b t" and
-  ListT_cases [elim!]: "x :\<^sub>b ListT a"
+  Bot\<^isub>v_cases [elim]: "Bot\<^isub>v a :\<^sub>b t" and
+  Bot\<^isub>t_cases [elim!]: "x :\<^sub>b Bot\<^isub>t" and
+  Bool\<^isub>v_cases [elim]: "Bool\<^isub>v x :\<^sub>b t" and
+  Bool\<^isub>t_cases [elim!]: "x :\<^sub>b Boot\<^isub>t" and
+  Int\<^isub>v_cases [elim]: "Int\<^isub>v x :\<^sub>b t" and
+  Int\<^isub>t_cases [elim!]: "x :\<^sub>b Int\<^isub>t" and
+  List\<^isub>v_cases [elim]: "List\<^isub>v a xs :\<^sub>b t" and
+  List\<^isub>t_cases [elim!]: "x :\<^sub>b List\<^isub>t a"
 
 instantiation bval :: DEFINED
 begin
 
-definition "Defined_bval (x::bval) = True"
+fun Defined_bval :: "bval \<Rightarrow> bool" where
+"\<D>(Bot\<^isub>v a) = False" |  "\<D>(Bool\<^isub>v x) = True" | "\<D>(Int\<^isub>v x) = True" |
+"\<D>(List\<^isub>v a xs) = (\<forall> x \<in> set(xs). \<D>(x))"
 
 instance ..
 end
-
-declare Defined_bval_def [simp]
 
 instantiation bval :: VALUE
 begin
@@ -55,7 +57,8 @@ definition utype_rel_bval :: "bval \<Rightarrow> nat \<Rightarrow> bool" where
 instance
   apply (intro_classes)
   apply (simp add:utype_rel_bval_def)
-  apply (rule_tac x="IntV 0" in exI)
+  apply (rule_tac x="to_nat(Int\<^isub>t)" in exI)
+  apply (rule_tac x="Int\<^isub>v 0" in exI)
   apply (auto)
 done
 end
@@ -67,11 +70,11 @@ lemma prjTYPE_inv_bty [simp]
 lemma embTYPE_inv_bty [simp]:
   "prjTYPE (embTYPE (t :: btyp) :: bval UTYPE) = t"
   apply (induct t)
-  apply (rule embTYPE_inv[of "BoolV False"])
+  apply (rule embTYPE_inv[of "Int\<^isub>v 0"])
   apply (auto simp add: utype_rel_bval_def)
-  apply (rule embTYPE_inv[of "IntV 0"])
+  apply (rule embTYPE_inv[of "Bool\<^isub>v False"])
   apply (auto simp add: utype_rel_bval_def)
-  apply (rule embTYPE_inv[of "ListV []"])
+  apply (rule_tac v="List\<^isub>v t []" in embTYPE_inv)
   apply (auto simp add: utype_rel_bval_def)
 done
 
@@ -83,18 +86,18 @@ instantiation bval :: BOOL_SORT
 begin
 
 definition MkBool_bval :: "bool \<Rightarrow> bval" where
-"MkBool_bval x = BoolV x"
+"MkBool_bval x = Bool\<^isub>v x"
 
 primrec DestBool_bval :: "bval \<Rightarrow> bool" where
-"DestBool_bval (BoolV x) = x" 
+"DestBool_bval (Bool\<^isub>v x) = x" 
 
 definition BoolType_bval :: "bval UTYPE" where
-"BoolType_bval = embTYPE BoolT"
+"BoolType_bval = embTYPE Bool\<^isub>t"
 
 instance
   apply (intro_classes)
-  apply (simp add:MkBool_bval_def DestBool_bval_def BoolType_bval_def dcarrier_def monotype_def Defined_bval_def type_rel_def)
-  apply (auto simp add:MkBool_bval_def DestBool_bval_def BoolType_bval_def Defined_bval_def dcarrier_def monotype_def)
+  apply (simp add:MkBool_bval_def DestBool_bval_def BoolType_bval_def dcarrier_def monotype_def type_rel_def)
+  apply (auto simp add:MkBool_bval_def DestBool_bval_def BoolType_bval_def dcarrier_def monotype_def)
   apply (metis prjTYPE_inv_bty)
 done
 end
@@ -103,18 +106,18 @@ instantiation bval :: INT_SORT
 begin
 
 definition MkInt_bval :: "int \<Rightarrow> bval" where
-"MkInt_bval x = IntV x"
+"MkInt_bval x = Int\<^isub>v x"
 
 primrec DestInt_bval :: "bval \<Rightarrow> int" where
-"DestInt_bval (IntV x) = x" 
+"DestInt_bval (Int\<^isub>v x) = x" 
 
 definition IntType_bval :: "bval UTYPE" where
-"IntType_bval = embTYPE IntT"
+"IntType_bval = embTYPE Int\<^isub>t"
 
 instance
   apply (intro_classes)
-  apply (simp add:MkInt_bval_def DestInt_bval_def IntType_bval_def dcarrier_def monotype_def Defined_bval_def type_rel_def)
-  apply (auto simp add:MkInt_bval_def DestInt_bval_def IntType_bval_def Defined_bval_def dcarrier_def monotype_def)
+  apply (simp add:MkInt_bval_def DestInt_bval_def IntType_bval_def dcarrier_def monotype_def type_rel_def)
+  apply (auto simp add:MkInt_bval_def DestInt_bval_def IntType_bval_def dcarrier_def monotype_def)
 done
 end
 
@@ -148,11 +151,11 @@ abbreviation
 
 lemma GCD_pre_sat_GCD_INV:
   "\<lbrakk> x > 0; y > 0 \<rbrakk> \<Longrightarrow> `($X > \<guillemotleft>0\<guillemotright>) \<and> ($Y > \<guillemotleft>0\<guillemotright>) \<and> (gcd($X,$Y) = gcd(\<guillemotleft>x\<guillemotright>,\<guillemotleft>y\<guillemotright>))` \<sqsubseteq> `($X = \<guillemotleft>x\<guillemotright> \<and> $Y = \<guillemotleft>y\<guillemotright>)`"
-  by (utp_pred_auto_tac)
+  by (utp_poly_auto_tac)
 
 lemma GCD_post_sat_GCD_INV:
   "\<lbrakk> x > 0; y > 0 \<rbrakk> \<Longrightarrow> `($X > \<guillemotleft>0\<guillemotright>) \<and> ($Y > \<guillemotleft>0\<guillemotright>) \<and> (gcd($X,$Y) = gcd(\<guillemotleft>x\<guillemotright>,\<guillemotleft>y\<guillemotright>))` \<sqsubseteq> `$X = $Y \<and> $X = gcd(\<guillemotleft>x\<guillemotright>,\<guillemotleft>y\<guillemotright>)`"
-  apply (utp_pred_auto_tac)
+  apply (utp_poly_auto_tac)
   apply (metis gcd_pos_int less_int_code(1))
   apply (metis abs_gcd_int)
 done
@@ -173,7 +176,7 @@ proof -
     apply (rule HoareP_AssignR)
     apply (force intro:closure unrest simp add:erasure typing defined)
     apply (simp add:usubst typing defined)
-    apply (utp_pred_auto_tac)
+    apply (utp_poly_auto_tac)
     apply (smt gcd_add1_int)
     apply (smt gcd_add1_int)
     apply (simp add:closure)
@@ -182,7 +185,7 @@ proof -
     apply (rule HoareP_AssignR)
     apply (force intro:closure unrest simp add:erasure typing defined)+
     apply (simp add:usubst typing defined)
-    apply (utp_pred_auto_tac)
+    apply (utp_poly_auto_tac)
     apply (smt gcd_add2_int)
     apply (smt gcd_add2_int)
     apply (simp add:closure)
@@ -192,7 +195,7 @@ proof -
 
   moreover from assms 
   have "`$X = $Y \<and> $X = gcd(\<guillemotleft>x\<guillemotright>,\<guillemotleft>y\<guillemotright>)` \<sqsubseteq> ?I \<and>\<^sub>p `$X = $Y` "
-    by (utp_pred_auto_tac)
+    by (utp_poly_auto_tac)
 
   ultimately show ?thesis
     apply (rule_tac HoareP_post_refine)
