116984e4b2b9034243fa7f1a3c40a4fee0cf150a
simonf@CS.YORK.AC.UK <simonf@CS.YORK.AC.UK@9de38aea-ec91-4ea3-8f56-587be14e846f>
2014-05-22 16:45:40 +0000

    Completed the conversion

--- theories/models/utp_cml/utp_cml_process.thy
+++ theories/models/utp_cml/utp_cml_process.thy
@@ -47,61 +47,61 @@ definition Exec0D :: "cmlp \<Rightarrow> cmlp" where
 
 lift_definition Exec1D :: 
   "('a option \<Rightarrow> cmlp) \<Rightarrow> 'a cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e. {b :: cmlv WF_BINDING. b \<in> P (e b)}" .
+  is "\<lambda> P e. {b :: cmlv binding. b \<in> P (e b)}" .
 
 lift_definition Exec2D :: 
   "('a option \<Rightarrow> 'b option \<Rightarrow> cmlp) \<Rightarrow> 'a cmle \<Rightarrow> 'b cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e f. {b :: cmlv WF_BINDING. b \<in> P (e b) (f b)}" .
+  is "\<lambda> P e f. {b :: cmlv binding. b \<in> P (e b) (f b)}" .
 
 lift_definition Exec3D :: 
   "('a option \<Rightarrow> 'b option \<Rightarrow> 'c option \<Rightarrow> cmlp) 
    \<Rightarrow> 'a cmle \<Rightarrow> 'b cmle \<Rightarrow> 'c cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e f g. {b :: cmlv WF_BINDING. b \<in> P (e b) (f b) (g b)}" .
+  is "\<lambda> P e f g. {b :: cmlv binding. b \<in> P (e b) (f b) (g b)}" .
 
 lift_definition Exec4D :: 
   "('a option \<Rightarrow> 'b option \<Rightarrow> 'c option \<Rightarrow> 'd option \<Rightarrow> cmlp) 
    \<Rightarrow> 'a cmle \<Rightarrow> 'b cmle \<Rightarrow> 'c cmle \<Rightarrow> 'd cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e f g h. {b :: cmlv WF_BINDING. b \<in> P (e b) (f b) (g b) (h b)}" .
+  is "\<lambda> P e f g h. {b :: cmlv binding. b \<in> P (e b) (f b) (g b) (h b)}" .
 
 lift_definition Exec5D :: 
   "('a option \<Rightarrow> 'b option \<Rightarrow> 'c option \<Rightarrow> 'd option \<Rightarrow> 'e option \<Rightarrow> cmlp) 
    \<Rightarrow> 'a cmle \<Rightarrow> 'b cmle \<Rightarrow> 'c cmle \<Rightarrow> 'd cmle \<Rightarrow> 'e cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e f g h i. {b :: cmlv WF_BINDING. b \<in> P (e b) (f b) (g b) (h b) (i b)}" .
+  is "\<lambda> P e f g h i. {b :: cmlv binding. b \<in> P (e b) (f b) (g b) (h b) (i b)}" .
 
 lift_definition Exec6D :: 
   "('a option \<Rightarrow> 'b option \<Rightarrow> 'c option \<Rightarrow> 'd option \<Rightarrow> 'e option \<Rightarrow> 'f option \<Rightarrow> cmlp) 
    \<Rightarrow> 'a cmle \<Rightarrow> 'b cmle \<Rightarrow> 'c cmle \<Rightarrow> 'd cmle \<Rightarrow> 'e cmle \<Rightarrow> 'f cmle \<Rightarrow> cmlp" 
-  is "\<lambda> P e f g h i j. {b :: cmlv WF_BINDING. b \<in> P (e b) (f b) (g b) (h b) (i b) (j b)}" .
+  is "\<lambda> P e f g h i j. {b :: cmlv binding. b \<in> P (e b) (f b) (g b) (h b) (i b) (j b)}" .
 
 (* We remove the standard definition of prefix and add one specific for CML *)
 
 no_syntax
-  "_upred_prefixed"  :: "pexpr \<Rightarrow> upred \<Rightarrow> upred" ("_ -> _")
+  "_n_upred_prefixed"  :: "n_pexpr \<Rightarrow> n_upred \<Rightarrow> n_upred" ("_ -> _")
 
 no_syntax (xsymbols)
-  "_upred_prefixed"  :: "pexpr \<Rightarrow> upred \<Rightarrow> upred" ("_ \<rightarrow> _")
+  "_n_upred_prefixed"  :: "n_pexpr \<Rightarrow> n_upred \<Rightarrow> n_upred" ("_ \<rightarrow> _")
 
 syntax
-  "_upred_cml_prefix" :: "unit option CHAN \<Rightarrow> upred \<Rightarrow> upred" ("_ -> _")
-  "_upred_parcml"    :: "upred \<Rightarrow> cmlv UCHAN set \<Rightarrow> upred \<Rightarrow> upred" (infixl "[|_|]" 50)
-  "_upred_cml_exec0" :: "idt \<Rightarrow> upred" ("_'(')")
-  "_upred_cml_exec1" :: "idt \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_')")
-  "_upred_cml_exec2" :: "idt \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_, _')")
-  "_upred_cml_exec3" :: "idt \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_, _, _')")
-  "_upred_cml_exec4" :: "idt \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_, _, _, _')")
-  "_upred_cml_exec5" :: "idt \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_, _, _, _, _')")
-  "_upred_cml_exec6" :: "idt \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> pexpr \<Rightarrow> upred" ("_'(_, _, _, _, _, _')")
+  "_n_upred_cml_prefix" :: "unit option CHAN \<Rightarrow> n_upred \<Rightarrow> n_upred" ("_ -> _")
+  "_n_upred_parcml"    :: "n_upred \<Rightarrow> cmlv UCHAN set \<Rightarrow> n_upred \<Rightarrow> n_upred" (infixl "[|_|]" 50)
+  "_n_upred_cml_exec0" :: "idt \<Rightarrow> n_upred" ("_'(')")
+  "_n_upred_cml_exec1" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_')")
+  "_n_upred_cml_exec2" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_, _')")
+  "_n_upred_cml_exec3" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_, _, _')")
+  "_n_upred_cml_exec4" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_, _, _, _')")
+  "_n_upred_cml_exec5" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_, _, _, _, _')")
+  "_n_upred_cml_exec6" :: "idt \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_pexpr \<Rightarrow> n_upred" ("_'(_, _, _, _, _, _')")
 
 translations
-  "_upred_parcml p vs q"        == "CONST ParallelD p vs q"
-  "_upred_cml_prefix n p"       == "CONST CommD n p"
-  "_upred_cml_exec0 s"          == "CONST RH (CONST Exec0D s)"
-  "_upred_cml_exec1 f s"        == "CONST RH (CONST Exec1D f s)"
-  "_upred_cml_exec2 v1 v2 s"    == "CONST RH (CONST Exec2D v1 v2 s)"
-  "_upred_cml_exec3 v1 v2 v3 s" == "CONST RH (CONST Exec3D v1 v2 v3 s)"
-  "_upred_cml_exec3 v1 v2 v3 v4 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 s)"
-  "_upred_cml_exec3 v1 v2 v3 v4 v5 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 v5 s)"
-  "_upred_cml_exec3 v1 v2 v3 v4 v5 v6 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 v5 v6 s)"
+  "_n_upred_parcml p vs q"        == "CONST ParallelD p vs q"
+  "_n_upred_cml_prefix n p"       == "CONST CommD n p"
+  "_n_upred_cml_exec0 s"          == "CONST RH (CONST Exec0D s)"
+  "_n_upred_cml_exec1 f s"        == "CONST RH (CONST Exec1D f s)"
+  "_n_upred_cml_exec2 v1 v2 s"    == "CONST RH (CONST Exec2D v1 v2 s)"
+  "_n_upred_cml_exec3 v1 v2 v3 s" == "CONST RH (CONST Exec3D v1 v2 v3 s)"
+  "_n_upred_cml_exec3 v1 v2 v3 v4 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 s)"
+  "_n_upred_cml_exec3 v1 v2 v3 v4 v5 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 v5 s)"
+  "_n_upred_cml_exec3 v1 v2 v3 v4 v5 v6 s" == "CONST RH (CONST Exec3D v1 v2 v3 v4 v5 v6 s)"
 
 term "`P [|{x,y,z}|] Q`"
 term "`f()`"
