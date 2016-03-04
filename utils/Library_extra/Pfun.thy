9878aecede0f1055b3d549a209b750e590545649
Simon Foster <simon.foster@york.ac.uk>
2016-03-04 14:02:33 +0000

    Added more laws and definitions

--- utils/Library_extra/Pfun.thy
+++ utils/Library_extra/Pfun.thy
@@ -1,12 +1,21 @@
+section {* Partial functions *}
+
 theory Pfun
 imports Map_Extra
 begin
+
+text {* I'm not completely satisfied with partial functions as provided by Map.thy, since they don't
+        have a unique type and so we can't instantiate classes, make use of adhoc-overloading
+        etc. Consequently I've created a new type and derived the laws. *}
 
 typedef ('a, 'b) pfun = "UNIV :: ('a \<rightharpoonup> 'b) set" ..
 
 setup_lifting type_definition_pfun
 
 lift_definition pfun_app :: "('a, 'b) pfun \<Rightarrow> 'a \<Rightarrow> 'b" ("_'(_')\<^sub>p" [999,0] 999) is "\<lambda> f x. the (f x)" .
+
+lift_definition pfun_upd :: "('a, 'b) pfun \<Rightarrow> 'a \<Rightarrow> 'b \<Rightarrow> ('a, 'b) pfun"
+is "\<lambda> f k v. f(k := Some v)" .
 
 lift_definition pdom :: "('a, 'b) pfun \<Rightarrow> 'a set" is dom .
 
@@ -43,8 +52,24 @@ lift_definition plus_pfun :: "('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun \<Righta
 instance ..
 end
 
+instantiation pfun :: (type, type) minus
+begin
+lift_definition minus_pfun :: "('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun" is "op --" .
+instance ..
+end
+
 instance pfun :: (type, type) monoid_add
   by (intro_classes, (transfer, auto)+)
+
+instantiation pfun :: (type, type) inf
+begin
+lift_definition inf_pfun :: "('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun" is
+"\<lambda> f g x. if (x \<in> dom(f) \<inter> dom(g) \<and> f(x) = g(x)) then f(x) else None" .
+instance ..
+end
+
+abbreviation pfun_inter :: "('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun" (infixl "\<inter>\<^sub>p" 80)
+where "pfun_inter \<equiv> inf"
 
 instantiation pfun :: (type, type) order
 begin
@@ -61,6 +86,9 @@ where "pfun_subset \<equiv> less"
 
 abbreviation pfun_subset_eq :: "('a, 'b) pfun \<Rightarrow> ('a, 'b) pfun \<Rightarrow> bool" (infix "\<subseteq>\<^sub>p" 50)
 where "pfun_subset_eq \<equiv> less_eq"
+
+instance pfun :: (type, type) semilattice_inf
+  by (intro_classes, (transfer, auto simp add: map_le_def dom_def)+)
 
 lemma pfun_ext: "\<lbrakk> \<And> x y. (x, y) \<in>\<^sub>p f \<longleftrightarrow> (x, y) \<in>\<^sub>p g \<rbrakk> \<Longrightarrow> f = g"
   by (transfer, simp add: map_ext)
@@ -87,7 +115,7 @@ lemma pfun_comp_left_id [simp]: "pId \<circ>\<^sub>p f = f"
 lemma pfun_comp_right_id [simp]: "f \<circ>\<^sub>p pId = f"
   by (transfer, auto)
 
-lemma 
+lemma pfun_override_dist_comp:
   "(f + g) \<circ>\<^sub>p h = (f \<circ>\<^sub>p h) + (g \<circ>\<^sub>p h)"
   apply (transfer)
   apply (rule ext)
@@ -128,5 +156,47 @@ lemma pdom_res_dist: "(A \<lhd>\<^sub>p f) \<rhd>\<^sub>p B = A \<lhd>\<^sub>p (
 lemma pdom_res_twice [simp]: "A \<lhd>\<^sub>p (B \<lhd>\<^sub>p f) = (A \<inter> B) \<lhd>\<^sub>p f"
   by (transfer, auto simp add: Int_commute)
 
+lemma pfun_member_plus:
+  "(x, y) \<in>\<^sub>p f + g \<longleftrightarrow> ((x \<notin> pdom(g) \<and> (x, y) \<in>\<^sub>p f) \<or> (x, y) \<in>\<^sub>p g)"
+  by (transfer, simp add: map_member_plus)
 
+lemma pfun_member_minus:
+  "(x, y) \<in>\<^sub>p f - g \<longleftrightarrow> (x, y) \<in>\<^sub>p f \<and> (\<not> (x, y) \<in>\<^sub>p g)"
+  by (transfer, simp add: map_member_minus)
+
+lemma pfun_minus_unit [simp]:
+  fixes f :: "('a, 'b) pfun"
+  shows "f - 0 = f"
+  by (transfer, simp add: map_minus_def)
+
+lemma pfun_minus_zero [simp]:
+  fixes f :: "('a, 'b) pfun"
+  shows "0 - f = 0"
+  by (transfer, simp add: map_minus_def)
+
+lemma pfun_minus_plus_commute:
+  "pdom(g) \<inter> pdom(h) = {} \<Longrightarrow> (f - g) + h = (f + h) - g"
+  by (transfer, simp add: map_minus_plus_commute)
+
+lemma pfun_minus_common_subset:
+  "\<lbrakk> h \<subseteq>\<^sub>p f; h \<subseteq>\<^sub>p g \<rbrakk> \<Longrightarrow> (f - h = g - h) = (f = g)"
+  by (transfer, simp add: map_minus_common_subset)
+
+lemma pfun_graph_inv: "graph_pfun (pfun_graph f) = f"
+  by (transfer, simp)
+
+lemma pfun_graph_zero: "pfun_graph 0 = {}"
+  by (transfer, simp add: map_graph_def)
+
+lemma pfun_graph_pId_on: "pfun_graph (pId_on A) = Id_on A"
+  by (transfer, auto simp add: map_graph_def)
+
+lemma pfun_graph_minus: "pfun_graph (f - g) = pfun_graph f - pfun_graph g"
+  by (transfer, simp add: map_graph_minus)
+
+lemma pfun_graph_inter: "pfun_graph (f \<inter>\<^sub>p g) = pfun_graph f \<inter> pfun_graph g"
+  apply (transfer, auto simp add: map_graph_def)
+  apply (metis option.discI)+
+done
+  
 end
