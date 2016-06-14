e8bd1d6951bf7d16f4305bcf99034e317837f93c
Simon Foster <simon.foster@york.ac.uk>
2016-06-14 16:24:23 +0100

    Added some more primitives for manipulating state variables

--- utp/utp_state.thy
+++ utp/utp_state.thy
@@ -1,4 +1,4 @@
-section {* Program variables *}
+section {* Program and observational variables *}
 
 theory utp_state
   imports utp_rel
@@ -17,20 +17,44 @@ type_synonym ('\<Sigma>, '\<O>) hrel = "('\<Sigma>, '\<O>) state_scheme hrelatio
 definition palpha :: "('\<Sigma>, '\<O>) palpha" ("\<^bold>v") where
 "palpha = VAR palpha\<^sub>u"
 
+definition obs :: "('\<Sigma>, '\<O>) oalpha" ("\<O>") where
+"obs = VAR more"
+
+definition state_make :: "'\<Sigma> \<times> '\<O> \<Longrightarrow> ('\<Sigma>, '\<O>) state_scheme" where
+"state_make = \<lparr> lens_get = \<lambda> v. (palpha\<^sub>u v, more v)
+              , lens_put = \<lambda> s v. \<lparr> palpha\<^sub>u = fst v, \<dots> = snd v \<rparr> \<rparr>"
+
+lemma state_make_bij:
+  "bij_lens state_make"
+  by (unfold_locales, simp_all add: state_make_def)
+(*
+definition state_map :: 
+  "('\<Sigma>\<^sub>1 \<Longrightarrow> '\<Sigma>\<^sub>2) \<Rightarrow> ('\<O>\<^sub>1 \<Longrightarrow> '\<O>\<^sub>2) \<Rightarrow> (('\<Sigma>\<^sub>1, '\<O>\<^sub>1) state_scheme \<Longrightarrow> ('\<Sigma>\<^sub>2, '\<O>\<^sub>2) state_scheme)" where
+"state_map = "
+*) 
+
 syntax
-  "_svid_palpha"  :: "svid" ("\<^bold>v")
+  "_svid_palpha" :: "svid" ("\<^bold>v")
+  "_svid_obs"    :: "svid" ("\<O>")
 
 translations
   "_svid_palpha" => "CONST palpha"
+  "_svid_obs" => "CONST obs"
 
-type_synonym ('a, '\<alpha>) lvar = "('a list, '\<alpha>) uvar"
+lemma palpha_uvar [simp]: "uvar \<^bold>v"
+  by (unfold_locales, simp_all add: palpha_def)
 
-type_synonym ('a, '\<Sigma>, '\<O>) uproc = "'a \<Rightarrow> ('\<Sigma>, '\<O>) hrel"
+lemma obs_uvar [simp]: "uvar \<O>"
+  by (unfold_locales, simp_all add: obs_def)
 
-definition 
-  val_parm :: "('a::two, '\<Sigma>) lvar \<Rightarrow> (('a, '\<Sigma>) uvar \<Rightarrow> ('\<Sigma>, '\<O>) hrel) 
-               \<Rightarrow> (('a, '\<alpha>) uexpr, '\<Sigma>, '\<O>) uproc"
-where "val_parm x P = undefined"
+lemma palpha_obs_indep [simp]:
+  "\<^bold>v \<bowtie> \<O>" "\<O> \<bowtie> \<^bold>v"
+  by (auto intro: lens_indepI simp add: palpha_def obs_def)
 
+lemma palpha_obs_bij: "bij_lens (\<^bold>v +\<^sub>L \<O>)"
+  by (unfold_locales, simp_all add: lens_plus_def prod.case_eq_if obs_def palpha_def)
+
+lemma palpha_obs_state: "\<^bold>v +\<^sub>L \<O> \<approx>\<^sub>L 1\<^sub>L"
+  using bij_lens_equiv_id palpha_obs_bij by blast
 
 end
