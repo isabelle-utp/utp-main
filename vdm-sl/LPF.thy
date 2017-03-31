e8343daa4b05791ce9894717c9f0c65d2a6f9fad
Casper Thule <casper.thule@eng.au.dk>
2017-03-31 10:44:21 +0100

    Updated LPF

--- vdm-sl/LPF.thy
+++ vdm-sl/LPF.thy
@@ -4,7 +4,7 @@
 (* Authors: Casper Thule and Frank Zeyda                                      *)
 (* Emails: casper.thule@eng.au.dk and frank.zeyda@york.ac.uk                  *)
 (******************************************************************************)
-(* LAST REVIEWED: 21 Mar 2017 *)
+(* LAST REVIEWED: 22 Mar 2017 *)
 
 section {* Logic of Partial Functions *}
 
@@ -13,6 +13,10 @@ imports Main Eisbach utp
 begin
 
 subsection {* LPF Type *}
+
+  
+
+
 
 text {*
   Below we define a new type to represent values in LPF. Effectively, we encode
@@ -23,86 +27,83 @@ text {*
 typedef 'a lpf = "UNIV :: 'a option set"
 apply(rule UNIV_witness)
 done
-print_theorems
-(* The type synonym below should really go into the VDM.thy theory. *)
 
-(* At a later point we want to introduce a UTP expression type for VDM. *)
+text {*
+  The two theorems lpf_defs and lpf_transfer are collection of proofs.
+  lpf_defs contains LPF definition aximos, such as a definition of definedness.
+  lpf_transfer contains LPF transfer laws, such as lifting values of HOL types 
+  into values of the LPF type
+*}
 
-type_synonym ('a, '\<alpha>) vexpr = "('a lpf, '\<alpha>) uexpr"
-
-(* With the below, pretty-printing of the type synonym works as well! *)
-
-typ "('a, '\<alpha>) vexpr"
-
-translations (type) "('a, '\<alpha>) vexpr" \<leftharpoondown> (type) "('a lpf, '\<alpha>) uexpr"
-
-typ "('a, '\<alpha>) vexpr"
-
-text {*Two theorems to use in proofs are set up  *}
 named_theorems lpf_defs "lpf definitional axioms"
 named_theorems lpf_transfer "lpf transfer laws"
-
+  
 lemmas Abs_lpf_inject_sym = sym [OF Abs_lpf_inject]
 lemmas Rep_lpf_inject_sym = sym [OF Rep_lpf_inject]
-
+  
 declare Rep_lpf_inverse [lpf_transfer]
 declare Abs_lpf_inverse [simplified, lpf_transfer]
 declare Rep_lpf_inject_sym [lpf_transfer]
 declare Abs_lpf_inject [simplified, lpf_transfer]
 
-text {*The lifting of the type lpf is set up*}
+text {* The lifting of values into the type @{type lpf} is set up *}
 setup_lifting type_definition_lpf
 
-text {*Lifting of a HOL type into a defined lpf value*}
-(*TODO: How can one define this as a function e.g. lpf_Some x = Some x*)
-lift_definition lpf_Some :: "'a \<Rightarrow> 'a lpf"
-is "Some" .
-print_theorems
+lift_definition lpf_the :: "'a lpf \<Rightarrow> 'a" is "(\<lambda>x . (the \<circ> Rep_lpf) x)" .
 
-text {* Lpf value for undefined *}
-lift_definition lpf_None :: "'a lpf"
-is "None" .
+text {* Here we define how to lift a value into a defined @{type lpf} value *}
 
-text {* Definition of definedness for lpf values*}
+lift_definition lpf_Some :: "'a \<Rightarrow> 'a lpf" is "Some" .
+
+declare lpf_Some.rep_eq [lpf_transfer]
+
+text {* The LPF value for undefined *}
+
+lift_definition lpf_None :: "'a lpf" is "None" .
+
+declare lpf_None.rep_eq [lpf_transfer]
+
+text {* 
+  This is a definition of definedness for LPF values.  
+  A LPF value is defined if it is not @{const lpf_None}.   
+*}
 definition defined :: "'a lpf \<Rightarrow> bool" ("\<D>'(_')") where
 "defined x \<longleftrightarrow> (x \<noteq> lpf_None)"
 
-declare lpf_Some.rep_eq [lpf_transfer]
-declare lpf_None.rep_eq [lpf_transfer]
 declare defined_def [lpf_defs]
+
+subsection {* Lifting of unary operators *}
 
 text {*
   The following function checks if an input x satisfies a predicate (belongs to
-  a set). If it does then it returns the value backed, wrapped up in the
+  a set). If it does then it returns the value backed, wrapped up in the 
   @{type lpf} type, otherwise it returns @{const lpf_None}.
 *}
 
 definition lpfSat :: "'a set \<Rightarrow> 'a \<Rightarrow> 'a lpf" where
 "lpfSat u a = (if a\<in>u then lpf_Some a else lpf_None)"
 
-text {* Overloading definition for monadic bind *}
+declare lpfSat_def [lpf_defs]
+
+text {* 
+  The following function is an overload of the bind operator for the lpf type 
+*}
+
 definition lift1_bind :: "'a lpf \<Rightarrow> ('a \<Rightarrow> 'b lpf) \<Rightarrow> 'b lpf" where
-[lpf_defs]: "lift1_bind a f =
+[lpf_defs]: "lift1_bind a f = 
   (if \<D>(a) then  (f \<circ> the \<circ> Rep_lpf) a else lpf_None)"
 
 adhoc_overloading
 bind lift1_bind
 
-text {* lift1_lpf takes a set which is a predicate on the input values,
-  a total HOL function taking one argument and turns  it into a function on
-  option types. The resulting value is defined if (1) each input is defined
-  and (2) the input satisfies the predicate.
+text {* lift1_lpf takes a set, which is a predicate on the input values, 
+  a total HOL function taking one argument and turns it into a function on 
+  @{type lpf} types. The resulting value is defined if (1) each input is defined
+  and (2) the input satisfies the predicate. 
 *}
 
-definition lift1_lpf_old :: "'a set \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf)" where
-[lpf_defs]:"lift1_lpf_old u f = (\<lambda>x . (x\<bind>lpfSat u)\<bind> lpf_Some \<circ> f)"
-
-definition lift1_lpf'_old :: "('a \<Rightarrow> 'b) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf)" where
-[lpf_defs]:"lift1_lpf'_old = lift1_lpf_old UNIV"
-
-lift_definition lift1_lpf :: "'a set \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf)" is
-"(\<lambda> u f x . (x\<bind>lpfSat u)\<bind> lpf_Some \<circ> f)"
-done
+lift_definition lift1_lpf :: "'a set \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf)" is 
+"(\<lambda> u f x . (x\<bind>lpfSat u)\<bind> lpf_Some \<circ> f)" .
 
 declare lift1_lpf.transfer [lpf_transfer]
 declare lift1_lpf_def [lpf_defs]
@@ -114,93 +115,37 @@ done
 declare lift1_lpf'.transfer [lpf_transfer]
 declare lift1_lpf'_def [lpf_defs]
 
-text {*
-  Below we define unary operators on the lpf type
-  using the lifting defined above.
-*}
+subsection {* Lifting of binary operators *}
 
-(* With instantiation, use the type class syntax for uminus. *)
-(*instantiation lpf :: (uminus) uminus
-begin
-definition uminus_lpf :: "'a lpf \<Rightarrow> 'a lpf" where
-"uminus_lpf = lift1_lpf' uminus"
-instance ..
-end*)
-
-definition uminus_lpf :: "'a::uminus lpf \<Rightarrow> 'a lpf" ("-\<^sub>L _" [81] 80) where
-[lpf_defs]: "uminus_lpf = lift1_lpf' uminus"
-
-definition card_lpf :: "'a set lpf \<Rightarrow> nat lpf" where
-[lpf_defs]: "card_lpf = lift1_lpf' card"
-
-definition abs_lpf :: "real lpf \<Rightarrow> real lpf" where
-[lpf_defs]: "abs_lpf = lift1_lpf' abs"
-
-definition floor_lpf :: "real lpf \<Rightarrow> int lpf" where
-[lpf_defs]: "floor_lpf = lift1_lpf' floor"
-
-definition len_lpf :: "'a list lpf \<Rightarrow> nat lpf" where
-[lpf_defs]: "len_lpf = lift1_lpf' length"
-
--- {* \todo{Define the following operators:
-  Boolean type: Negation
-  Record types: field select and is.
-  Set unary operators: dunion, dinter and power.
-  Sequence unary operators: hd, tl, elems, inds, reverse, conc and indexing.
-  Map unary operators: dom, rng, merge, apply and inverse.  }
-*}
-
-lift_definition lift2_lpf ::
+text {* Lifting of binary operators *}
+lift_definition lift2_lpf :: 
   "('a * 'b) set \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf)" is
-  "(\<lambda> u f v1 v2.
-  do { x \<leftarrow> v1; y \<leftarrow> v2; lpfSat u (x, y) } \<bind> lpf_Some \<circ> uncurry f)"
-done
+  "(\<lambda> u f v1 v2. 
+  do { x \<leftarrow> v1; y \<leftarrow> v2; lpfSat u (x, y) } \<bind> lpf_Some \<circ> uncurry f)" .
 
 declare lift2_lpf.transfer [lpf_transfer]
 declare lift2_lpf_def [lpf_defs]
 
-lift_definition lift2_lpf' ::
-  "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf)" is
+lift_definition lift2_lpf' :: 
+  "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf)" is 
   "lift2_lpf UNIV"
 done
 
 declare lift2_lpf'.transfer [lpf_transfer]
 declare lift2_lpf'_def [lpf_defs]
 
-text {*
-  Lifts a HOL function with two arguments into a function on the lpf type
-*}
-definition lift2_lpf_old ::
-  "('a * 'b) set \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf)" where
-[lpf_defs]: "lift2_lpf_old u f =
-  (\<lambda> v1 v2. do { x \<leftarrow> v1; y \<leftarrow> v2; lpfSat u (x, y) } \<bind> lpf_Some \<circ> uncurry f)"
+subsection {* Lifting of ternary operators *}
 
-definition lift2_lpf'_old :: "('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf)" where
-[lpf_defs]:"lift2_lpf'_old = lift2_lpf UNIV"
+lift_definition lift3_lpf :: 
+  "('a * 'b * 'c) set \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> 'd) \<Rightarrow> ('a lpf \<Rightarrow> 'b lpf \<Rightarrow> 'c lpf \<Rightarrow> 'd lpf)" is
+"(\<lambda> ABC f v1 v2 v3. do { x \<leftarrow> v1; y \<leftarrow> v2; z \<leftarrow> v3; lpfSat ABC (x, y, z) } \<bind> lpf_Some \<circ> (\<lambda> (x,y,z). f x y z))" .
 
-(*TODO: How should this be done? First lift into lpf', and then lift into vexpr?"*)
-consts lift1_vdm :: "'a set \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> (('a, '\<alpha>) vexpr \<Rightarrow> ('b, '\<alpha>) vexpr)"
-(* Open question: do we really need two layers of lifting? *)
-
-declare lift1_lpf'_def [lpf_defs]
-
-(* Without instantiation, define your own syntax. *)
-
-definition uminus_lpf :: "'a::uminus lpf \<Rightarrow> 'a lpf" ("-\<^sub>L _" [81] 80) where
-[lpf_defs]: "uminus_lpf = lift1_lpf' uminus"
-
-(* With instantiation, use the type class syntax for uminus. *)
-(*instantiation lpf :: (uminus) uminus
-begin
-definition uminus_lpf :: "'a lpf \<Rightarrow> 'a lpf" where
-"uminus_lpf = lift1_lpf' uminus"
-instance ..
-end
-*)
-
+text {*  Three tactics are created for use in proofs. *}
 method lpf_simp = (simp add: lpf_defs lpf_transfer; clarsimp?)
 method lpf_auto = (lpf_simp; auto)
 method lpf_blast = (lpf_simp; blast)
+                                                                        
+subsection {* Examples *}
 
 lemma "lpf_Some x = lpf_Some y \<longleftrightarrow> x = y"
   by (metis lpf_Some.rep_eq option.inject)
@@ -209,9 +154,8 @@ lemma "lpf_Some x = lpf_Some y \<longleftrightarrow> x = y"
 apply (lpf_simp)
 done
 
-(*How do I prove these?*)
 lemma all_lpf_transfer [lpf_transfer]:
-"(\<forall>x::'a lpf. P x) = (\<forall>x::'a option. P (Abs_lpf x))"
+"(\<forall>x::'a lpf. P x) = (\<forall>x::'a option. P (Abs_lpf x))" 
 apply (safe)
 -- {* Subgoal 1 *}
 apply (drule_tac x = "Abs_lpf x" in spec)
@@ -221,7 +165,6 @@ apply (drule_tac x = "Rep_lpf x" in spec)
 apply (simp add: Rep_lpf_inverse)
 done
 
-thm exI
 lemma ex_lpf_transfer [lpf_transfer]:
 "(\<exists>x::'a lpf. P x) = (\<exists>x::'a option. P (Abs_lpf x))"
 apply (safe)
@@ -234,7 +177,7 @@ apply (assumption)
 done
 
 lemma meta_lpf_transfer [lpf_transfer]:
-"(\<And>x::'a lpf. P x) \<equiv> (\<And>x::'a option. P (Abs_lpf x))"
+"(\<And>x::'a lpf. P x) \<equiv> (\<And>x::'a option. P (Abs_lpf x))" 
 apply (rule)
 -- {* Subgoal 1 *}
 apply (drule_tac x = "Abs_lpf x" in meta_spec)
@@ -244,12 +187,13 @@ apply (drule_tac x = "Rep_lpf x" in meta_spec)
 apply (simp add: Rep_lpf_inverse)
 done
 
-lemma lift1_lpf'_example: "lift1_lpf' card lpf_None = lpf_None"
+lemma lifted_card_undefined_example: "lift1_lpf' card lpf_None = lpf_None"
 apply (lpf_simp)
 done
 
-lemma lift1_lpf'_example: "lift1_lpf' card (lpf_Some {1,2,3}) = lpf_Some 3"
+lemma lifted_card_defined_example: "lift1_lpf' card (lpf_Some ({1,2,3}::nat set)) = lpf_Some 3"
 apply (lpf_simp)
-(* We need ex_lpf_transfer *)
-oops
+done
+
+
 end
---
+++ vdm-sl/LPF_VDM.thy
@@ -0,0 +1,100 @@
+(******************************************************************************)
+(* Project: The Isabelle/UTP Proof System                                     *)
+(* File: LPF_VDM.thy                                                          *)
+(* Authors: Casper Thule, Frank Zeyda and Simon Foster                        *)
+(* Emails: casper.thule@eng.au.dk and {frank.zeyda, simon.foster}@york.ac.uk  *) 
+(******************************************************************************)
+(* LAST REVIEWED: 22 Mar 2017 *)
+
+section {* Embedding of VDM *}
+
+theory LPF_VDM
+imports LPF "../utp/models/utp_deep"
+begin
+
+text {* 
+  A type synonym is set up for improved syntax. 
+  A @{text vexpr} is a function that takes a state as argument and returns the 
+  value of the expression evaluated in the given state. E.g. a state can be 
+  @{text "{x \<leadsto> 2}"} and if an expression @{text "x + 2"} is evaluated in this 
+  state, then it would yield 4.
+*} 
+
+type_synonym ('a, '\<alpha>) vexpr = "('a lpf, '\<alpha>) uexpr"
+
+text {* 
+  The translation below makes for pretty printing.
+  Before: @{text "('a, '\<alpha>) vexpr"} prints @{text "('a lpf, '\<alpha>) uexpr"}.
+  After: @{text "('a, '\<alpha>) vexpr"} prints @{text "('a, '\<alpha>) vexpr"}.
+*}  
+
+translations (type) "('a, '\<alpha>) vexpr" \<leftharpoondown> (type) "('a lpf, '\<alpha>) uexpr"
+
+text {* The lifting of values into the type @{type uexpr} is set up next. *}
+
+setup_lifting type_definition_uexpr
+
+text {* 
+  The function below determines undefinedness. It returns @{const lpf_None} 
+  regardless of the argument.
+*}
+
+definition undefined_vdm :: "('a, '\<alpha>) vexpr" where
+"undefined_vdm = \<guillemotleft>lpf_None\<guillemotright>"
+
+text {* The function below determines definedness. Its argument is a function
+  that takes an argument. If the value produced by appliying the argument to the 
+  function is defined, then true is returned. Otherwise false is returned.
+*}
+
+definition defined_vdm :: "('a, '\<sigma>) vexpr \<Rightarrow> (bool, '\<sigma>) uexpr" where
+"defined_vdm = (uop defined)"
+
+text {*
+  The function below takes a HOL variable and returns a binding. 
+  When a value is applied to the binding it returns the HOL variable wrapped in 
+  the LPF defined. It is not necessary to check for definedness, as HOL 
+  variables are defined.
+*}
+
+definition lit_vdm :: "'a \<Rightarrow> ('a, '\<sigma>) vexpr" where
+"lit_vdm c = \<guillemotleft>lpf_Some c\<guillemotright>"
+
+text {*
+  In the function below we lift a unary operator into the type @{type vexpr}.
+  The function takes a predicate and the function to lift. It lifts the function 
+  into a binding, which when given a value, returns the value produced by the 
+  binding.
+*}
+
+lift_definition lift1_vdm_new :: 
+  "('a \<Rightarrow> bool) \<Rightarrow> ('a \<Rightarrow> 'b)  \<Rightarrow> (('a, '\<sigma>) vexpr \<Rightarrow> ('b, '\<sigma>) vexpr)" is
+" (\<lambda>(p::'c\<Rightarrow>bool) (f::'c => 'd) (uf::'e=>'c lpf) (e::'e) .
+  if ((defined \<circ> uf) e) \<and> (p \<circ> lpf_the \<circ> uf) e 
+  then ((lift1_lpf' f) \<circ> uf) e 
+  else lpf_None)" .
+
+lift_definition lift1_vdm_new' :: 
+  "('a \<Rightarrow> 'b)  \<Rightarrow> (('a, '\<sigma>) vexpr \<Rightarrow> ('b, '\<sigma>) vexpr)" is
+" (\<lambda>(f::'c => 'd) . lift1_vdm_new (\<lambda>x . True) f)" .
+
+lift_definition hd_vdm :: "('a list, '\<sigma>) vexpr \<Rightarrow> ('a, '\<sigma>) vexpr" is
+"lift1_vdm_new (\<lambda>x . x\<noteq>[]) hd" .
+
+lift_definition tl_vdm :: "('a list, '\<sigma>) vexpr \<Rightarrow> ('a list, '\<sigma>) vexpr" is
+"lift1_vdm_new' tl" .
+
+consts bop_vdm ::
+  "('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow>
+   ('a \<Rightarrow> 'b \<Rightarrow> 'c) \<Rightarrow>
+   (('a, '\<sigma>) uexpr \<Rightarrow> ('b, '\<sigma>) vexpr \<Rightarrow> ('c, '\<sigma>) vexpr)"
+
+consts trop_vdm ::
+  "('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> bool) \<Rightarrow>
+   ('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> 'd) \<Rightarrow>
+   (('a, '\<sigma>) vexpr \<Rightarrow> ('b, '\<sigma>) vexpr \<Rightarrow> ('c, '\<sigma>) vexpr \<Rightarrow> ('d, '\<sigma>) vexpr)"
+
+text {* Finally, we also need to lift lenses into VDM expressions. *}
+
+text {* Once we have done this, we have to reconfigure the proof strategy! *}
+end
