0dc7437c5fa483fa8ceb1fd7628a07a8ccd6c6c1
Lrke <lrke.inh@gmail.com>
2017-04-10 21:02:04 +0200

    infix operatorer + few examples

--- vdm-sl/LPF_Operators.thy
+++ vdm-sl/LPF_Operators.thy
@@ -25,6 +25,8 @@ theory LPF_Operators
 imports LPF Transcendental
 begin recall_syntax
 
+no_notation Lens_Order.sublens  (infix "\<subseteq>\<^sub>L" 55)
+  
 text {*
   Below we define unary operators on the @{type lpf} type using the lifting 
   function @{const lift1_lpf'}.
@@ -241,7 +243,7 @@ definition seq_mod_lpf :: "'a list lpf \<Rightarrow> (nat, 'a) map lpf \<Rightar
 
 definition seq_index_lpf :: "'a list lpf \<Rightarrow> nat lpf \<Rightarrow> 'a lpf" where
 [lpf_defs]: "seq_index_lpf = lift2_lpf {(x,y) . y < (length x) \<and> y > 0} (op !)"
-
+ 
 
 subsubsection {* Comprehensions *}
 (*
@@ -294,10 +296,134 @@ apply(simp add: defined_def)
 done
 
 syntax
+(* Unary Operators *)
+(* Boolean Unary Operators *)
+"_lpfnot" :: "bool lpf \<Rightarrow> bool lpf" ("\<not>\<^sub>L _" [40] 40)
+(* Numeric Unary Operators *)
+"_lpfabs" :: "real lpf \<Rightarrow> real lpf" ("\<bar>_\<bar>\<^sub>L") 
+"_lpffloor" :: "real lpf \<Rightarrow> int lpf" ("\<lfloor>_\<rfloor>\<^sub>L") 
+"_lpflen" :: "'a list lpf \<Rightarrow> nat lpf" ("len\<^sub>L'(_')")
+(* Set Unary Operators *)
+"_lpfcard" :: "'a set lpf \<Rightarrow> nat lpf" ("card\<^sub>L'(_')")
+"_lpfdunion" :: "'a set set lpf \<Rightarrow> 'a set lpf" ("\<Union>\<^sub>L")
+"_lpfdinter" :: "'a set set lpf \<Rightarrow> 'a set lpf" ("\<Inter>\<^sub>L")
+"_lpfpower" :: "'a set lpf \<Rightarrow> 'a set set lpf" ("Pow\<^sub>L'(_')")
+(* Sequence Unary Operators *)
+"_lpfhd" :: "'a list lpf \<Rightarrow> 'a lpf" ("hd\<^sub>L'(_')")
+"_lpftl" :: "'a list lpf \<Rightarrow> 'a list lpf" ("tl\<^sub>L'(_')")
+"_lpfelems" :: "'a list lpf \<Rightarrow> 'a set lpf" ("elem\<^sub>L'(_')")
+"_lpfinds" :: "'a list lpf \<Rightarrow> nat set lpf" ("inds\<^sub>L'(_')")
+"_lpfreverse" :: "'a list lpf \<Rightarrow> 'a list lpf" ("reverse\<^sub>L'(_')")
+"_lpfconc" :: "'a list list lpf \<Rightarrow> 'a list lpf" ("conc\<^sub>L'(_')")
+(* Map Unary Operators *)
+"_lpfdom" :: "('a,'b) map lpf \<Rightarrow> 'a set lpf" ("dom\<^sub>L'(_')")
+"_lpfrng" :: "('a,'b) map lpf \<Rightarrow> 'b set lpf" ("rng\<^sub>L'(_')")
+"_lpfmerge" :: "('a, 'b) map set lpf \<Rightarrow> ('a, 'b) map lpf" ("merge\<^sub>L'(_')")
+(* Binary Operators *)
+(* Polymorphic Binary Operators *)
+"_lpfequal" :: "'a lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix "=\<^sub>L" 50) 
+"_lpfnot_equal" :: "'a lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix "\<noteq>\<^sub>L" 50)
+(* Boolean Binary Operators  *)
+"_lpfconj" :: "bool lpf \<Rightarrow> bool lpf \<Rightarrow> bool lpf" (infixr "\<and>\<^sub>L" 35)
+"_lpfdisj" :: "bool lpf \<Rightarrow> bool lpf \<Rightarrow> bool lpf" (infixr "\<or>\<^sub>L" 30)
+"_lpfimplies" :: "bool lpf \<Rightarrow> bool lpf \<Rightarrow> bool lpf" (infixr "\<Rightarrow>\<^sub>L" 25)
+"_lpfbiimplication" :: "bool lpf \<Rightarrow> bool lpf \<Rightarrow> bool lpf" (infixr "\<Leftrightarrow>\<^sub>L" 25)
+(* Numeric Binary Operators  *)
+"_lpfdiv" :: "int lpf \<Rightarrow> int lpf \<Rightarrow> int lpf" (infixl "div\<^sub>L" 70)
+"_lpfrem" :: "int lpf \<Rightarrow> int lpf \<Rightarrow> int lpf" (infixl "rem\<^sub>L" 70) 
+"_lpfmod" :: "int lpf \<Rightarrow> int lpf \<Rightarrow> int lpf" (infixl "mod\<^sub>L" 70)
+"_lpfpower_nat" :: "'a::power lpf \<Rightarrow> nat lpf \<Rightarrow> 'a lpf" (infixr "^\<^sub>L" 80)
+"_lpfpower_real" :: "'a::ln lpf \<Rightarrow> 'a lpf \<Rightarrow> 'a lpf" (infixr "powr\<^sub>L" 80)
+"_lpfslt" :: "'a::ord lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix "<\<^sub>L" 50)
+"_lpflte" :: "'a::ord lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix "\<le>\<^sub>L" 50)
+"_lpfsgt" :: "'a::ord lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix ">\<^sub>L" 50) 
+"_lpfgte" :: "'a::ord lpf \<Rightarrow> 'a lpf \<Rightarrow> bool lpf" (infix "\<ge>\<^sub>L" 50)
+(* Record Binary Operators *)
+(* Set Binary Operators *)
+"_lpfin" :: "'a lpf \<Rightarrow> 'a set lpf \<Rightarrow> bool lpf" (infix "\<in>\<^sub>L" 50)
+"_lpfnot_in" :: "'a lpf \<Rightarrow> 'a set lpf \<Rightarrow> bool lpf" (infix "\<notin>\<^sub>L" 50)
 "_lpfunion" :: "'a set lpf \<Rightarrow> 'a set lpf \<Rightarrow> 'a set lpf" (infixl "\<union>\<^sub>L" 65)
+"_lpfintersect" :: "'a set lpf \<Rightarrow> 'a set lpf \<Rightarrow> 'a set lpf" (infixl "\<inter>\<^sub>L" 70)
+"_lpfdifference" :: "'a set lpf \<Rightarrow> 'a set lpf \<Rightarrow> 'a set lpf" ("\<setminus>\<^sub>L")
+"_lpfsubset" :: "'a set lpf \<Rightarrow> 'a set lpf \<Rightarrow> bool lpf" (infix "\<subseteq>\<^sub>L" 50)
+"_lpfpsubset" :: "'a set lpf \<Rightarrow> 'a set lpf \<Rightarrow> bool lpf" (infix "\<subset>\<^sub>L" 50)
+(* Sequence Binary Operators *)
+"_lpfconc_seq" :: "'a list lpf \<Rightarrow> 'a list lpf \<Rightarrow> 'a list lpf" ("conc\<^sub>L")
+"_lpfseq_mod" :: "'a list lpf \<Rightarrow> (nat, 'a) map lpf \<Rightarrow> 'a list lpf" ("++\<^sub>L")
+"_lpfseq_index" :: "'a list lpf \<Rightarrow> nat lpf \<Rightarrow> 'a lpf" ("inds\<^sub>L")
+(* Comprehensions *)
 
 translations
-"A \<union>\<^sub>L B"   == "CONST union_lpf A B"
+(* Unary Operators *)
+(* Boolean Unary Operators *)
+"\<not>\<^sub>L p" == "CONST not_lpf p" 
+(* Numeric Unary Operators *)
+"\<bar>x\<bar>\<^sub>L" == "CONST abs_lpf x"
+"\<lfloor>x\<rfloor>\<^sub>L" == "CONST floor_lpf x"
+"len\<^sub>L(xs)" == "CONST len_lpf xs"
+(* Set Unary Operators *)
+"card\<^sub>L(f)" == "CONST card_lpf f"
+"\<Union>\<^sub>L A" == "CONST dunion_lpf A"
+"\<Inter>\<^sub>L A" == "CONST dinter_lpf A"
+"Pow\<^sub>L(A)" == "CONST power_lpf A"
+(* Sequence Unary Operators *)
+"hd\<^sub>L(xs)" == "CONST hd_lpf xs"
+"tl\<^sub>L(xs)" == "CONST tl_lpf xs"
+"elem\<^sub>L(xs)" == "CONST elems_lpf xs"
+"inds\<^sub>L(xs)" == "CONST inds_lpf xs"
+"reverse\<^sub>L(xs)" == "CONST reverse_lpf xs"
+"conc\<^sub>L(xs)" == "CONST conc_lpf xs"
+(* Map Unary Operators *)
+"dom\<^sub>L(f)" == "CONST dom_lpf f"
+"rng\<^sub>L(f)" == "CONST rng_lpf f"
+"merge\<^sub>L(f)" == "CONST merge_lpf f"
+(* Binary Operators *)
+(* Polymorphic Binary Operators *)
+"x =\<^sub>L y" == "CONST equal_lpf x y"
+"x \<noteq>\<^sub>L y" == "CONST not_equal_lpf x y"
+(* Boolean Binary Operators  *)
+"p \<and>\<^sub>L q" == "CONST conj_lpf p q"
+"p \<or>\<^sub>L q" == "CONST disj_lpf p q"
+"p \<Rightarrow>\<^sub>L q" == "CONST implies_lpf p q"
+"p \<Leftrightarrow>\<^sub>L q" == "CONST biimplication_lpf p q"
+(* Numeric Binary Operators  *)
+"x div\<^sub>L y" == "CONST div_lpf x y"
+"x rem\<^sub>L y" == "CONST rem_lpf x y"
+"x mod\<^sub>L y" == "CONST mod_lpf x y"
+"x ^\<^sub>L y" == "CONST power_nat_lpf x y"
+"x powr\<^sub>L y" == "CONST power_real_lpf x y"
+"x >\<^sub>L y" == "CONST slt_lpf x y"
+"x \<ge>\<^sub>L y" == "CONST lte_lpf x y"
+"x <\<^sub>L y" == "CONST sgt_lpf x y"
+"x \<le>\<^sub>L y" == "CONST gte_lpf x y"
+(* Record Binary Operators *)
+(* Set Binary Operators *)
+"x \<in>\<^sub>L A" == "CONST in_lpf x A"
+"x \<notin>\<^sub>L A" == "CONST not_in_lpf x A"
+"A \<union>\<^sub>L B" == "CONST union_lpf A B"
+"A \<inter>\<^sub>L B" == "CONST intersect_lpf A B"
+"A \<setminus>\<^sub>L B" == "CONST difference_lpf A B"
+"A \<subset>\<^sub>L B" == "CONST psubset_lpf A B"
+"A \<subseteq>\<^sub>L B" == "CONST subset_lpf A B"
+(* Sequence Binary Operators *)
+"xs conc\<^sub>L ys" == "CONST conc_seq_lpf xs ys"
+"xs ++\<^sub>L ys" == "CONST seq_mod_lpf xs ys"
+"xs inds\<^sub>L x" == "CONST seq_index_lpf xs x"
+(* Comprehensions *)
+    
+(*This is not going to work with =\<^sub>L*)
+lemma "(x \<and>\<^sub>L x) = x"
+  using [[simp_trace=true]]
+  apply(lpf_auto)
+done
 
+lemma "(x \<or>\<^sub>L x) = x"
+  using [[simp_trace=true]]
+  apply(lpf_auto)
+done
 
+lemma "(p  \<and>\<^sub>L q) = (q  \<and>\<^sub>L p )"
+apply(lpf_auto)
+done      
+  
 end
