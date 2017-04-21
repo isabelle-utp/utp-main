7b54b0342205affe6c14d8f43229787c9fb6cde0
Simon Foster <simon.foster@york.ac.uk>
2017-04-21 16:57:46 +0100

    Added "meta-substitution" which allows us to consider substitution of HOL variables with UTP expressions

---
+++ utp/utp_meta_subst.thy
@@ -0,0 +1,37 @@
+section {* Meta-level substitution *}
+
+theory utp_meta_subst
+imports utp_rel
+begin
+  
+definition msubst :: "('a \<Rightarrow> '\<alpha> upred) \<Rightarrow> ('a, '\<alpha>) uexpr \<Rightarrow> '\<alpha> upred" where
+[upred_defs]: "msubst F v = (\<Sqinter> x | \<guillemotleft>x\<guillemotright> =\<^sub>u v \<bullet> F(x))"
+
+syntax
+  "_msubst"   :: "logic \<Rightarrow> id \<Rightarrow> logic \<Rightarrow> logic" ("(_\<lbrakk>_\<rightarrow>_\<rbrakk>)" [990,0,0] 991)
+
+translations
+  "_msubst P x v" => "CONST msubst (\<lambda> x. P) v"
+ 
+lemma msubst_true [usubst]: "true\<lbrakk>x\<rightarrow>v\<rbrakk> = true"
+  by (pred_auto)
+
+lemma msubst_false [usubst]: "false\<lbrakk>x\<rightarrow>v\<rbrakk> = false"
+  by (pred_auto)
+    
+lemma msubst_lit [usubst]: "\<guillemotleft>x\<guillemotright>\<lbrakk>x\<rightarrow>v\<rbrakk> = v"
+  by (pred_auto)
+
+lemma msubst_not [usubst]: "(\<not> P(x))\<lbrakk>x\<rightarrow>v\<rbrakk> = (\<not> ((P x)\<lbrakk>x\<rightarrow>v\<rbrakk>))"
+  by (pred_auto)
+    
+lemma msubst_disj [usubst]: "(P(x) \<or> Q(x))\<lbrakk>x\<rightarrow>v\<rbrakk> = ((P(x))\<lbrakk>x\<rightarrow>v\<rbrakk> \<or> (Q(x))\<lbrakk>x\<rightarrow>v\<rbrakk>)"
+  by (pred_auto)
+    
+lemma msubst_conj [usubst]: "(P(x) \<and> Q(x))\<lbrakk>x\<rightarrow>v\<rbrakk> = ((P(x))\<lbrakk>x\<rightarrow>v\<rbrakk> \<and> (Q(x))\<lbrakk>x\<rightarrow>v\<rbrakk>)"
+  by (pred_auto)
+  
+lemma msubst_seq [usubst]: "(P(x) ;; Q(x))\<lbrakk>x\<rightarrow>\<guillemotleft>v\<guillemotright>\<rbrakk> = ((P(x))\<lbrakk>x\<rightarrow>\<guillemotleft>v\<guillemotright>\<rbrakk> ;; (Q(x))\<lbrakk>x\<rightarrow>\<guillemotleft>v\<guillemotright>\<rbrakk>)"
+  by (rel_auto)  
+  
+end
