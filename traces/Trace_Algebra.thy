98dc90705e98fb980d2980729903e9fd9f869e87
Simon Foster <simon.foster@york.ac.uk>
2018-02-05 13:55:28 +0000

    Added theory for trace algebras

---
+++ traces/Trace_Algebra.thy
@@ -0,0 +1,269 @@
+section {* Trace Algebras *}
+
+theory Trace_Algebra
+  imports List_extra Positive
+begin
+
+class ordered_semigroup = semigroup_add + order +
+  assumes add_left_mono: "a \<le> b \<Longrightarrow> c + a \<le> c + b"
+  and add_right_mono: "a \<le> b \<Longrightarrow> a + c \<le> b + c"
+begin
+
+lemma add_mono:
+  "a \<le> b \<Longrightarrow> c \<le> d \<Longrightarrow> a + c \<le> b + d"
+  using local.add_left_mono local.add_right_mono local.order.trans by blast
+
+end
+
+class left_cancel_monoid = monoid_add +
+  assumes add_left_imp_eq: "a + b = a + c \<Longrightarrow> b = c"
+
+class right_cancel_monoid = monoid_add +
+  assumes add_right_imp_eq: "b + a = c + a \<Longrightarrow> b = c"
+
+class monoid_sum_0 = monoid_add +
+  assumes zero_sum_left: "a + b = 0 \<Longrightarrow> a = 0"
+begin
+
+lemma zero_sum_right: "a + b = 0 \<Longrightarrow> b = 0"
+  by (metis local.add_0_left local.zero_sum_left)
+
+lemma zero_sum: "a + b = 0 \<longleftrightarrow> a = 0 \<and> b = 0"
+  by (metis local.add_0_right zero_sum_right)
+
+end
+
+context monoid_add
+begin
+
+definition monoid_le (infix "\<le>\<^sub>m" 50)
+where "a \<le>\<^sub>m b \<longleftrightarrow> (\<exists>c. b = a + c)"
+
+definition monoid_subtract (infixl "-\<^sub>m" 65)
+where "a -\<^sub>m b = (if (b \<le>\<^sub>m a) then THE c. a = b + c else 0)"
+
+end 
+
+class pre_trace = left_cancel_monoid + monoid_sum_0 +
+  assumes
+  sum_eq_sum_conv: "(a + b) = (c + d) \<Longrightarrow> \<exists> e . a = c + e \<and> e + b = d \<or> a + e = c \<and> b = e + d"
+begin
+  
+lemma monoid_le_least_zero: "0 \<le>\<^sub>m a"
+  by (simp add: monoid_le_def)
+
+lemma monoid_le_refl: "a \<le>\<^sub>m a"
+  by (simp add: monoid_le_def, metis add.right_neutral)
+
+lemma monoid_le_trans: "\<lbrakk> a \<le>\<^sub>m b; b \<le>\<^sub>m c \<rbrakk> \<Longrightarrow> a \<le>\<^sub>m c"
+  by (metis add.assoc monoid_le_def)
+
+lemma monoid_le_antisym:
+  assumes "a \<le>\<^sub>m b" "b \<le>\<^sub>m a"
+  shows "a = b"
+proof -
+  obtain a' where a': "b = a + a'"
+    using assms(1) monoid_le_def by auto
+
+  obtain b' where b': "a = b + b'"
+    using assms(2) monoid_le_def by auto
+
+  have "b' = (b' + a' + b')"
+    by (metis a' add_assoc b' local.add_left_imp_eq)
+
+  hence "a' + b' = 0"
+    by (metis add_assoc local.add_0_right local.add_left_imp_eq)
+
+  hence "a' = 0" "b' = 0"
+    by (simp add: zero_sum)+
+
+  with a' b' show ?thesis
+    by simp
+qed
+
+lemma monoid_le_add: "a \<le>\<^sub>m a + b"
+  by (auto simp add: monoid_le_def)
+
+lemma monoid_le_add_left_mono: "a \<le>\<^sub>m b \<Longrightarrow> c + a \<le>\<^sub>m c + b"
+  using add_assoc by (auto simp add: monoid_le_def)
+
+lemma add_monoid_diff_cancel_left [simp]: "(a + b) -\<^sub>m a = b"
+  apply (simp add: monoid_subtract_def monoid_le_add)
+  apply (rule the_equality)
+  apply (simp)
+  using local.add_left_imp_eq apply blast
+done
+
+end
+  
+class trace = pre_trace + ord + minus +
+  assumes le_is_monoid_le: "a \<le> b \<longleftrightarrow> (a \<le>\<^sub>m b)"
+  and less_iff: "a < b \<longleftrightarrow> a \<le> b \<and> \<not> (b \<le> a)"
+  and minus_def: "a - b = a -\<^sub>m b"
+begin
+
+  lemma le_iff_add: "a \<le> b \<longleftrightarrow> (\<exists> c. b = a + c)"
+    by (simp add: local.le_is_monoid_le local.monoid_le_def)
+
+  lemma least_zero [simp]: "0 \<le> a"
+    by (simp add: local.le_is_monoid_le local.monoid_le_least_zero)
+
+  lemma le_add [simp]: "a \<le> a + b"
+    by (simp add: le_is_monoid_le local.monoid_le_add)
+
+  lemma not_le_minus [simp]:  "\<not> (a \<le> b) \<Longrightarrow> b - a = 0"
+    by (simp add: le_is_monoid_le local.minus_def local.monoid_subtract_def)
+
+  lemma add_diff_cancel_left [simp]: "(a + b) - a = b"
+    by (simp add: minus_def)
+
+  lemma diff_zero [simp]: "a - 0 = a"
+    by (metis local.add_0_left local.add_diff_cancel_left)
+
+  lemma diff_cancel [simp]: "a - a = 0"
+    by (metis local.add_0_right local.add_diff_cancel_left)
+
+  lemma add_left_mono: "a \<le> b \<Longrightarrow> c + a \<le> c + b"
+    by (simp add: local.le_is_monoid_le local.monoid_le_add_left_mono)
+
+  lemma add_le_imp_le_left: "c + a \<le> c + b \<Longrightarrow> a \<le> b"
+    by (auto simp add: le_iff_add, metis add_assoc local.add_diff_cancel_left)
+      
+  lemma add_diff_cancel_left' [simp]:  "(c + a) - (c + b) = a - b"
+  proof (cases "b \<le> a")
+    case True thus ?thesis
+      by (metis add_assoc local.add_diff_cancel_left local.le_iff_add)
+  next
+    case False thus ?thesis
+      using local.add_le_imp_le_left not_le_minus by blast
+  qed
+
+  lemma minus_zero_eq: "\<lbrakk> b \<le> a; a - b = 0 \<rbrakk> \<Longrightarrow> a = b"
+    using local.le_iff_add local.monoid_le_def by auto
+
+  lemma diff_add_cancel_left': "a \<le> b \<Longrightarrow> a + (b - a) = b"
+    using local.le_iff_add local.monoid_le_def by auto
+
+  lemma add_left_strict_mono: "\<lbrakk> a + b < a + c \<rbrakk> \<Longrightarrow> b < c"
+    using local.add_le_imp_le_left local.add_left_mono local.less_iff by blast
+      
+  lemma sum_minus_left: "c \<le> a \<Longrightarrow> (a + b) - c = (a - c) + b"
+    by (metis add_assoc diff_add_cancel_left' local.add_monoid_diff_cancel_left local.minus_def)      
+      
+  lemma neq_zero_impl_greater:
+    "x \<noteq> 0 \<Longrightarrow> 0 < x"
+    using le_is_monoid_le less_iff monoid_le_antisym monoid_le_least_zero by auto
+ 
+  lemma minus_cancel_le:
+    "\<lbrakk> x \<le> y; y \<le> z \<rbrakk> \<Longrightarrow> y - x \<le> z - x"
+    using add_assoc le_iff_add by auto
+
+  text {* The set subtraces of a common trace $c$ is totally ordered *} 
+  lemma le_common_total: "\<lbrakk> a \<le> c; b \<le> c \<rbrakk> \<Longrightarrow> a \<le> b \<or> b \<le> a"
+    by (metis diff_add_cancel_left' le_add local.sum_eq_sum_conv)
+  
+  lemma le_sum_cases: "a \<le> b + c \<Longrightarrow> a \<le> b \<or> b \<le> a"
+    by (simp add: le_common_total)
+            
+  lemma le_sum_cases':
+    "a \<le> b + c \<Longrightarrow> a \<le> b \<or> b \<le> a \<and> a - b \<le> c"
+    by (auto, metis le_sum_cases, metis minus_def le_is_monoid_le add_monoid_diff_cancel_left monoid_le_def sum_eq_sum_conv)
+
+  lemma le_sum_iff: "a \<le> b + c \<longleftrightarrow> a \<le> b \<or> b \<le> a \<and> a - b \<le> c"
+    by (metis le_sum_cases' add_monoid_diff_cancel_left le_is_monoid_le minus_def monoid_le_add_left_mono monoid_le_def monoid_le_trans)
+    
+  lemma sum_minus_right: "c \<ge> a \<Longrightarrow> a + b - c = b - (c - a)"
+    by (metis diff_add_cancel_left' local.add_diff_cancel_left')
+
+  lemma minus_gr_zero_iff [simp]:
+    "0 < x - y \<longleftrightarrow> y < x"
+    by (metis diff_cancel le_is_monoid_le least_zero less_iff minus_zero_eq monoid_le_antisym not_le_minus)
+
+  lemma le_zero_iff [simp]: "x \<le> 0 \<longleftrightarrow> x = 0"
+    using local.le_iff_add local.zero_sum by auto
+            
+  lemma minus_assoc [simp]: "x - y - z = x - (y + z)"
+    by (metis local.add_diff_cancel_left' local.diff_add_cancel_left' local.le_add local.le_sum_iff 
+        local.not_le_minus local.zero_sum_right)
+      
+end
+
+instance trace \<subseteq> order
+  apply (intro_classes)
+  apply (simp_all add: less_iff le_is_monoid_le monoid_le_refl)
+  using monoid_le_trans apply blast
+  apply (simp add: monoid_le_antisym)
+done
+
+instantiation list :: (type) monoid_add
+begin
+
+  definition zero_list :: "'a list" where "zero_list = []"
+  definition plus_list :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where "plus_list = op @"
+
+instance
+  by (intro_classes, simp_all add: zero_list_def plus_list_def)
+
+end
+
+lemma monoid_le_list:
+  "(xs :: 'a list) \<le>\<^sub>m ys \<longleftrightarrow> xs \<le> ys"
+  apply (simp add: monoid_le_def plus_list_def)
+  using Prefix_Order.prefixE Prefix_Order.prefixI apply blast
+done
+
+lemma monoid_subtract_list:
+  "(xs :: 'a list) -\<^sub>m ys = xs - ys"
+  apply (auto simp add: monoid_subtract_def monoid_le_list minus_list_def less_eq_list_def)
+  apply (rule the_equality)
+  apply (simp_all add: zero_list_def plus_list_def prefix_drop)
+done
+
+instance list :: (type) trace
+  apply (intro_classes, simp_all add: zero_list_def plus_list_def monoid_le_def monoid_subtract_list)
+  apply (simp add: append_eq_append_conv2)
+  using Prefix_Order.prefixE Prefix_Order.prefixI apply blast
+  apply (simp add: less_list_def)
+done
+
+lemma monoid_le_nat:
+  "(x :: nat) \<le>\<^sub>m y \<longleftrightarrow> x \<le> y"
+  by (simp add: monoid_le_def nat_le_iff_add)
+
+lemma monoid_subtract_nat:
+  "(x :: nat) -\<^sub>m y = x - y"
+  by (auto simp add: monoid_subtract_def monoid_le_nat)
+
+instance nat :: trace
+  apply (intro_classes, simp_all add: monoid_subtract_nat)
+  apply (metis Nat.diff_add_assoc Nat.diff_add_assoc2 add_diff_cancel_right' add_le_cancel_left add_le_cancel_right add_less_mono cancel_ab_semigroup_add_class.add_diff_cancel_left' less_irrefl not_le)
+  apply (simp add: nat_le_iff_add monoid_le_def)
+  apply linarith+
+done
+
+text {* Positives form a trace algebra *}
+    
+instance pos :: (linordered_semidom) trace
+proof (intro_classes, simp_all)
+  fix a b c d :: "'a pos"
+  show "a + b = 0 \<Longrightarrow> a = 0"
+    by (transfer, simp add: add_nonneg_eq_0_iff)
+  show "a + b = c + d \<Longrightarrow> \<exists>e. a = c + e \<and> e + b = d \<or> a + e = c \<and> b = e + d"
+    apply (cases "c \<le> a")
+    apply (metis (no_types, lifting) cancel_semigroup_add_class.add_left_imp_eq le_add_diff_inverse semiring_normalization_rules(25))
+    apply (metis (no_types, lifting) cancel_semigroup_add_class.add_left_imp_eq less_imp_le linordered_semidom_class.add_diff_inverse semiring_normalization_rules(21))
+  done
+  show "(a < b) = (a \<le> b \<and> \<not> b \<le> a)"
+    by auto    
+  show le_def: "\<And> a b :: 'a pos. (a \<le> b) = (a \<le>\<^sub>m b)"    
+    by (auto simp add: monoid_le_def, metis le_add_diff_inverse)  
+  show "a - b = a -\<^sub>m b"
+    apply (auto simp add: monoid_subtract_def le_def[THEN sym])
+     apply (rule sym)
+     apply (rule the_equality)
+      apply (simp_all)
+      apply (transfer, simp)
+  done
+qed
+
+end
