d9db8476769a7bbd579e6a7d916071f34de5b448
Simon Foster <simon.foster@york.ac.uk>
2018-08-23 12:39:10 +0100

    Finished adaptation for Isabelle 2018 and regenerated all documents

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ ISABELLE_UTP=${ISABELLE_UTP:-$(readlink -f $(dirname $0))/..}
 # Directory for scripts and binary executables.
 BIN_DIR=$ISABELLE_UTP/bin
 CONTRIB_DIR=$ISABELLE_UTP/contrib
-OPTICS_DIR=$ISABELLE_UTP/optics
 
 # Check for Isabelle/UTP dependencies
 bash "$BIN_DIR/utp_deps.sh"
@@ -21,7 +20,7 @@ heaps=( "UTP-Toolkit" "UTP" "UTP-KAT" "UTP-Designs" "UTP-Reactive" "UTP-Reactive
 
 for ((i=0;i<${#heaps[@]};++i));
 do
-	isabelle build -d $ROOT -d $OPTICS_DIR -d $CONTRIB_DIR -b "${heaps[i]}" || break
+	isabelle build -d $ROOT -d $CONTRIB_DIR -b "${heaps[i]}" || break
         if [ -f "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" ]; then
                 echo "Installing ${heaps[i]} documentation to doc/${heaps[i]}.pdf..."
 		cp "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" "$ISABELLE_UTP/doc/${heaps[i]}.pdf"
