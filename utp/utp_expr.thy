8dd20c5b5fc4c870d6100a1860684fb00c59c798
Simon Foster <simon.foster@york.ac.uk>
2019-04-01 12:42:02 +0100

    Added some extra option syntax

--- utp/utp_expr.thy
+++ utp/utp_expr.thy
@@ -194,6 +194,8 @@ syntax \<comment> \<open> Core expression constructs \<close>
   "_ulens_ovrd" :: "logic \<Rightarrow> logic \<Rightarrow> salpha \<Rightarrow> logic" ("_ \<oplus> _ on _" [85, 0, 86] 86)
   "_ulens_get"  :: "logic \<Rightarrow> svar \<Rightarrow> logic" ("_:_" [900,901] 901)
   "_umem"       :: "('a, '\<alpha>) uexpr \<Rightarrow> ('a set, '\<alpha>) uexpr \<Rightarrow> (bool, '\<alpha>) uexpr" (infix "\<in>\<^sub>u" 50)
+  "_uNone"      :: "logic" ("None\<^sub>u")
+  "_uSome"      :: "logic \<Rightarrow> logic" ("Some\<^sub>u'(_')")
   "_uthe"       :: "logic \<Rightarrow> logic" ("the\<^sub>u'(_')")
 
 translations
@@ -203,6 +205,8 @@ translations
   "_ulens_ovrd f g a" <= "CONST bop (\<lambda>x y. CONST lens_override x1 y1 a) f g"
   "_ulens_get x y" == "CONST uop (CONST lens_get y) x"
   "x \<in>\<^sub>u A" == "CONST bop (\<in>) x A"
+  "_uNone" == "CONST lit CONST None"
+  "_uSome e" == "CONST uop CONST Some e"
   "_uthe x" == "CONST uop (CONST the) x"
 
 syntax \<comment> \<open> Tuples \<close>
