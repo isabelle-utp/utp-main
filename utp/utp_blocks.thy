7b7e0b4d3e260481066ab2fa612babbdf47ac9d9
simondfoster <simondf@gmail.com>
2019-06-08 13:25:54 +0100

    Slightly altered definition of block open so that it nondeterministically assigns initial values. Added a few more algebraic laws.

--- utp/utp_blocks.thy
+++ utp/utp_blocks.thy
@@ -32,17 +32,33 @@ lemma subst_ext_apply [usubst]: "\<langle>ext\<^sub>s a\<rangle>\<^sub>s x = &x 
 subsection \<open> Generic Blocks \<close>
 
 definition block_open :: "('a \<Longrightarrow> 'b) \<Rightarrow> ('a, 'b) urel" ("open\<^bsub>_\<^esub>") where
-[upred_defs]: "block_open a = \<langle>ext\<^sub>s a\<rangle>\<^sub>a"
+[upred_defs]: "block_open a = \<langle>ext\<^sub>s a\<rangle>\<^sub>a ;; ($a\<acute> =\<^sub>u $a)"
 
 definition block_close :: "('a \<Longrightarrow> 'b) \<Rightarrow> ('b, 'a) urel" ("close\<^bsub>_\<^esub>") where
 [upred_defs]: "block_close a = \<langle>con\<^sub>s a\<rangle>\<^sub>a"
 
+lemma block_open_conv: 
+  "mwb_lens a \<Longrightarrow> open\<^bsub>a\<^esub>\<^sup>- = close\<^bsub>a\<^esub>"
+  by (rel_auto)
+
 lemma block_open_close:
   "mwb_lens a \<Longrightarrow> open\<^bsub>a\<^esub> ;; close\<^bsub>a\<^esub> = II"
-  by (rel_simp)
+  by (rel_auto, metis mwb_lens_def weak_lens.create_get)
+
+lemma block_open_open:
+  "\<lbrakk> mwb_lens a; mwb_lens b\<rbrakk> \<Longrightarrow> open\<^bsub>a\<^esub> ;; open\<^bsub>b\<^esub> = open\<^bsub>a ;\<^sub>L b\<^esub>"
+  by (rel_auto)
+
+lemma block_close_close:
+  "\<lbrakk> mwb_lens a; mwb_lens b\<rbrakk> \<Longrightarrow> close\<^bsub>a\<^esub> ;; close\<^bsub>b\<^esub> = close\<^bsub>b ;\<^sub>L a\<^esub>"
+  by (rel_auto)
 
 lemma block_assigns_open:
-  "mwb_lens a \<Longrightarrow> \<langle>\<sigma>\<rangle>\<^sub>a ;; open\<^bsub>a\<^esub> = open\<^bsub>a\<^esub> ;; \<langle>\<sigma> \<oplus>\<^sub>s a\<rangle>\<^sub>a"
-  by (rel_simp)
+  "vwb_lens a \<Longrightarrow> \<langle>\<sigma>\<rangle>\<^sub>a ;; open\<^bsub>a\<^esub> = open\<^bsub>a\<^esub> ;; \<langle>\<sigma> \<oplus>\<^sub>s a\<rangle>\<^sub>a"
+  by (rel_auto, metis lens_override_def lens_override_idem mwb_lens_axioms_def mwb_lens_def vwb_lens_def weak_lens.put_get)
+
+lemma block_assign_local_close:
+  "a \<bowtie> x \<Longrightarrow> x := v ;; close\<^bsub>a\<^esub> = close\<^bsub>a\<^esub>"
+  by (rel_auto)
 
 end
