066a2bc4550410e866e80325114823bd136cac84
Simon Foster <simon.foster@york.ac.uk>
2020-01-03 10:24:51 +0000

    Slight change to schema syntax

--- utp/utp_schema.thy
+++ utp/utp_schema.thy
@@ -2,7 +2,7 @@ section \<open> UTP Schema Types \<close>
 
 theory utp_schema
   imports "utp_definition"
-  keywords "schema" :: "thy_decl_block" and "invariants"
+  keywords "schema" :: "thy_decl_block"
 begin
 
 text \<open> Create a type with invariants attached; similar to a Z schema. \<close>
@@ -14,7 +14,7 @@ val _ =
   Outer_Syntax.command @{command_keyword schema} "define a new schema type"
     (Parse_Spec.overloaded -- (Parse.type_args_constrained -- Parse.binding) --
       (@{keyword "="} |-- Scan.option (Parse.typ --| @{keyword "+"}) --
-        Scan.repeat1 Parse.const_binding) -- Scan.optional (@{keyword "invariants"} |-- Scan.repeat1 Parse.term) ["true"]
+        Scan.repeat1 Parse.const_binding) -- Scan.optional (@{keyword "where"} |-- Scan.repeat1 Parse.term) ["true"]
     >> (fn (((overloaded, x), (y, z)), ts) =>
         let val n = Binding.name_of (snd x); open HOLogic in
         Toplevel.theory (Lens_Utils.add_alphabet_cmd {overloaded = overloaded} x y z
