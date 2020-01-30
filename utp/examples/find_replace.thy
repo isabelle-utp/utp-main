d18036c259bbc47cbd4200f2428258934e935699
Simon Foster <simon.foster@york.ac.uk>
2020-01-30 09:30:00 +0000

    Simplification of the verification

--- utp/examples/find_replace.thy
+++ utp/examples/find_replace.thy
@@ -11,35 +11,26 @@ alphabet ss =
   occ :: "nat"
   i   :: "nat"
 
-definition find_replace :: "(nat \<Rightarrow> string) \<Rightarrow> nat \<Rightarrow> string \<Rightarrow> string \<Rightarrow> ss hrel" where
+definition find_replace :: 
+  "(nat \<Rightarrow> string) \<Rightarrow> nat \<Rightarrow> string \<Rightarrow> string \<Rightarrow> ss hrel" where
 "find_replace inp len mtc rpl =
-  arr := inp ;;
-  i := 0 ;;
-  occ := 0 ;;
+  arr := inp ;; i := 0 ;; occ := 0 ;;
   while i < len 
   invr arr = (\<lambda> k. if k < i \<and> inp k = mtc then rpl else inp k)
-        \<and> occ = length (filter ((=) mtc) (map inp [0..<i]))
+     \<and> occ = length (filter ((=) mtc) (map inp [0..<i]))
   do
     if (arr i = mtc)
-    then 
-      arr[i] := rpl ;;
-      occ := occ + 1
-    else
-      arr[i] := inp i
+    then arr[i] := rpl ;; occ := occ + 1
+    else arr[i] := arr i
     fi ;;
     i := i + 1
   od"    
 
 lemma find_replace_correct:
-  "\<^bold>{true\<^bold>}find_replace inp len mtc rpl\<^bold>{arr = (\<lambda> k. if k < i \<and> inp k = mtc then rpl else inp k)\<^bold>}"
+  "\<^bold>{true\<^bold>}
+    find_replace inp len mtc rpl
+   \<^bold>{arr = (\<lambda> k. if k < i \<and> inp k = mtc then rpl else inp k)\<^bold>}"
   unfolding find_replace_def
-  apply (hoare_split)
-     apply (rel_simp')
-     apply (simp add: fun_eq_iff)
-    apply (rel_simp')
-  apply (simp add: fun_eq_iff)
-   apply (rel_simp')
-  apply (rel_simp')
-  done
+  by (hoare_split, rel_auto', metis less_Suc_eq)
 
 end
