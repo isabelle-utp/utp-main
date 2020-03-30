a2325bf2957307d3b855fe662734a2164da7ac6d
Simon Foster <simon.foster@york.ac.uk>
2020-03-30 12:06:36 +0100

    Added support for type variables and extension to the schema command.

--- utp/utp_schema.thy
+++ utp/utp_schema.thy
@@ -7,8 +7,6 @@ begin
 
 text \<open> Create a type with invariants attached; similar to a Z schema. \<close>
 
-(* TODO: Inherit invariants from extended schemas, add more robust type definition. *)
-
 ML \<open>
 val _ =
   Outer_Syntax.command @{command_keyword schema} "define a new schema type"
@@ -17,22 +15,34 @@ val _ =
         Scan.repeat1 Parse.const_binding) -- Scan.optional (@{keyword "where"} |-- Scan.repeat1 Parse.term) ["true"]
     >> (fn (((overloaded, x), (y, z)), ts) =>
         let val n = Binding.name_of (snd x)
-                   val invn = n ^ "_inv"
-                   val itb = Binding.make (invn ^ "_def", Position.none)               
-                   val ib = (SOME (Binding.make (invn, Position.none), SOME ("('a " ^ n ^ "_scheme) upred"), NoSyn))
+            val varl = fold (fn _ => fn y => "_, " ^ y) (1 upto length (fst x)) "'a" 
+            val invn = n ^ "_inv"
+            val itb = Binding.make (invn ^ "_def", Position.none)               
+            val upred = Lexicon.unmark_type @{type_syntax upred}
+            val ib = (SOME (Binding.make (invn, Position.none), SOME ("((" ^ varl ^ ")" ^ n ^ "_scheme) " ^ upred), NoSyn))
                  open HOLogic in
         Toplevel.theory
           (Lens_Utils.add_alphabet_cmd {overloaded = overloaded} x y z
            #> Named_Target.theory_map
               (fn ctx =>
-               let val invs = Library.foldr1 HOLogic.mk_conj (map (Syntax.parse_term ctx) ts) 
+               let val invs = Library.foldr1 HOLogic.mk_conj (map (Syntax.parse_term ctx) ts)
+                   val sinv = case y of
+                      NONE => invs |
+                      SOME t => case (Syntax.parse_typ ctx t) of
+                        Type (n, _) => (case (Syntax.parse_term ctx (n ^ "_inv")) of
+                          Const ("_type_constraint_", _) $ Const (n', _) => HOLogic.mk_conj (Const (n', dummyT), invs) | _ => invs) |
+                        _ => invs
                in 
-                 snd (UTP_Def.utp_def (itb, []) ib (mk_eq (Free (invn, dummyT), invs)) ctx)
+                 snd (UTP_Def.utp_def (itb, []) ib (mk_eq (Free (invn, dummyT), sinv)) ctx)
                end)
            #> Named_Target.theory_map 
               (fn ctx => 
                let val Const (cn, _) = Syntax.read_term ctx invn 
-                       val ty = Syntax.read_typ ctx (n ^ " upred") in
+                   val varl = 
+                     if (length (fst x) = 0)
+                     then ""
+                     else "(" ^ foldr1 (fn (x, y) => "_, " ^ x) (map (fn _ => "_") (1 upto length (fst x))) ^ ") "
+                   val ty = Syntax.read_typ ctx (varl ^ n ^ " " ^ upred) in
                Specification.abbreviation Syntax.mode_default (SOME (Binding.make (n, Position.none), SOME ty, NoSyn)) [] (Logic.mk_equals (Free (n, dummyT), Const (cn, dummyT))) false ctx
                end)
 )
