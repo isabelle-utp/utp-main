7386a80113bb6c41507af55506cc41ad1316d5ea
Simon Foster <simon.foster@york.ac.uk>
2021-10-21 13:51:23 +0100

    Updated the build system and regenerated most documentation.

--- bin/build.sh
+++ bin/build.sh
@@ -5,7 +5,6 @@ ISABELLE_UTP=${ISABELLE_UTP:-$(readlink -f $(dirname $0))/..}
 
 # Directory for scripts and binary executables.
 BIN_DIR=$ISABELLE_UTP/bin
-CONTRIB_DIR=$ISABELLE_UTP/contrib
 
 # Check for Isabelle/UTP dependencies
 bash "$BIN_DIR/utp_deps.sh"
@@ -21,7 +20,7 @@ heaps=( "UTP-Toolkit" "UTP" "UTP-KAT" "UTP-Designs" "UTP-Reactive" "UTP-Reactive
 
 for ((i=0;i<${#heaps[@]};++i));
 do
-	eval $ISABELLE build -d $ROOT -d $CONTRIB_DIR -b "${heaps[i]}" || break
+	eval $ISABELLE build -d $ROOT -b "${heaps[i]}" || break
         if [ -f "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" ]; then
                 echo "Installing ${heaps[i]} documentation to doc/${heaps[i]}.pdf..."
 		cp "$ISABELLE_UTP/${dirs[i]}/output/document.pdf" "$ISABELLE_UTP/doc/${heaps[i]}.pdf"
